1cae0d8e04ac6e857f88292cf7756c8a256fe7ff
Dany <danycambrano1@gmail.com>
2025-03-14 11:14:29 -0600

    Modal

---
+++ src/components/profile/CambioDatos/CambioDatos.tsx
@@ -0,0 +1,11 @@
+import {IonInput,IonButton} from '@ionic/react';
+import {useFormik} from "formik";
+import * as Yup from "yup"; 
+
+import "./CambioDatos.scss";
+
+export function CambioDatos() {
+  return (
+    <div>CambioDatos</div>
+  )
+}
--- src/components/profile/MenuOptions/MenuOptions.tsx
+++ src/components/profile/MenuOptions/MenuOptions.tsx
@@ -2,12 +2,18 @@ import {IonContent, IonModal} from '@ionic/react'
 import {imageOutline,personCircleOutline} from "ionicons/icons";
 import {Camera,CameraResultType,CameraSource} from "@capacitor/camera";
 import {useUser} from "../../../hooks";
-import {Options} from "./Options"
+import {Options} from "./Options";
+import {useRef} from "react";
+import {CambioDatos} from "../CambioDatos";
 
 import "./MenuOptions.scss";
 
 export function MenuOptions() {
+    const modalRef=useRef<HTMLIonModalElement>(null);
 const {onChangeAvatar} = useUser();
+
+const openName=()=>modalRef.current?.present();
+const closeModal=()=>modalRef.current?.dismiss();
 
     const openCamera=async()=>{
         const response= await Camera.getPhoto({
@@ -22,9 +28,16 @@ const {onChangeAvatar} = useUser();
         
     }
   return (
+    <>
     <div className='menu-options-container'>
         <Options titulo="Cambiar avatar" icon={imageOutline} onClick={openCamera}/>
         <Options titulo="Cambiar nombre" icon={personCircleOutline} onClick={()=>console.log("")}/>
     </div>
+    <IonModal trigger='open-modal' initialBreakpoint={0.35} breakpoints={[0,0.35]}>
+        <IonContent ref={modalRef} className='ion-paddin'>
+           {/*  <CambioDatos onClose={closeModal}/> */}
+        </IonContent>
+    </IonModal>
+    </>
   )
 }
